6e8245154651b0a521831e2f42200cc6b962aa38
Clof <urlocalcreator@gmail.com>
2023-12-01 17:59:34 -0900

    Update

--- Engine/Shaders/Default.py
+++ Engine/Shaders/Default.py
@@ -3,44 +3,21 @@ import numba as nb
 import numpy as np
 import math
 
+
 @nb.njit(nogil=True, fastmath=True)
 def cross(a, b):
     return np.array([a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0]])
+
 
 @nb.njit(nogil=True, fastmath=True)
 def normalize(a):
     norm_a = np.sqrt(np.sum(a**2))
     return a / norm_a if norm_a != 0 else a
 
+
 @nb.njit(nogil=True, fastmath=True)
 def shader(tri):
     return [normalize(cross(tri[1] - tri[0], tri[2] - tri[0]))]
-
-
-@nb.njit(nogil=True, fastmath=True)
-def pixel_filter(xyz, color, colors, dith):
-    colors = 255 / colors
-    if colors > 1:
-        color /= colors
-    if dith:
-        for i in nb.prange(len(color)):
-            j = color[i]
-            seed = 9834579
-            x, y, z = xyz
-            seed = seed * 315325887822453 + 141861939145533
-            x = x * 315325887822453 + 141861939145533
-            y = y * 315325887822453 + 141861939145533
-            z = z * 315325887822453 + 141861939145533
-            c = ((seed * x * y * z) % 999999999999999) / 999999999999999
-            color[i] = np.floor(j) if (c > j - np.floor(j)) else np.ceil(j)
-
-    color = np.round(color)
-
-    if colors > 1:
-        color = color * colors
-
-    color = np.minimum(255, color)
-    return color
 
 
 @nb.njit(nogil=True, fastmath=True)
